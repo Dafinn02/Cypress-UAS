4fd1019073679f434c558ede4d684e2dd8099f48
Dafinn02 <aping1647@gmail.com>
2023-12-19 13:53:46 +0700

    Cypress

---
+++ tests/cypress/integration/edit-user.cy.js
@@ -0,0 +1,67 @@
+describe('Test User can login to system', () => {
+
+  it('user can login with valid username and password', () => {
+    cy.visit('http://127.0.0.1:8000/');
+    cy.get(':nth-child(2) > .form-control').type('superadmin@gmail.com');
+    cy.get(':nth-child(3) > .form-control').type('password');
+    cy.get('.btn').click();
+
+    cy.get(".nav-link > .d-sm-none").should("have.text", "Hi, SuperAdmin");
+  });
+
+  it("user cannot with valid username and wrong password", () => {
+// arrage
+cy.visit('http://127.0.0.1:8000/');
+//act
+    cy.get(":nth-child(2) > .form-control").type("superadmin@gmail.com");
+    cy.get(":nth-child(3) > .form-control").type("password-salah");
+    cy.get(".btn").click();
+
+    cy.get(".invalid-feedback").should(
+      "have.text", 
+      "These credentials do not match our records."
+      );
+
+  });
+  it("user cannot login with empty username and valid password", () => {
+
+    cy.visit('http://127.0.0.1:8000/');
+
+    cy.get(":nth-child(2) > .form-control").type("superadminadasda@gmail.com");
+    cy.get(":nth-child(3) > .form-control").type("password");
+    cy.get(".btn").click();
+
+    cy.get(".invalid-feedback").should(
+      "have.text", 
+      "These credentials do not match our records."
+      );
+
+    });
+    it("user cannot login with empty username and correct password", () => {
+
+    cy.visit('http://127.0.0.1:8000/');
+
+    cy.get(":nth-child(3) > .form-control").type("password");
+    cy.get(".btn").click();
+    
+    cy.get(".invalid-feedback").should(
+      "have.text", 
+      "The email field is required."
+      );
+
+
+    }); 
+    it("user cannot login with empty username and correct password", () => {
+
+      cy.visit('http://127.0.0.1:8000/');
+  
+      cy.get(":nth-child(2) > .form-control").type("superadminad@gmail.com");
+      cy.get(".btn").click();
+
+      cy.get(".invalid-feedback").should(
+        "have.text", 
+        "The password field is required."
+        );
+  
+      }); 
+  });
---
+++ tests/cypress/integration/user-exesting-data.cy.js
@@ -0,0 +1,28 @@
+describe('User Can EditExisting Data', () => {
+  beforeEach(() =>{
+    //reset database using cypress command
+    //cy.exec("cd ../demo-app-cypress-automation && php artisan migrate:fresh --seed");
+    //arrange
+  cy.visit('http://127.0.0.1:8000/');
+  //act
+  cy.get(':nth-child(2) > .form-control').type('superadmin@gmail.com');
+  cy.get(':nth-child(3) > .form-control').type('password');
+  cy.get('.btn').click();
+  cy.visit("http://127.0.0.1:8000/user-management/user");
+    });
+//positive test case
+it('User can edit existing data', () => {
+  cy.get(".table td")
+  .contains("user")
+  .parent()
+  .find("a")
+  .contains("Edit")
+  .click();
+  cy.get("#name").clear("user");
+  cy.get('#name').type('user edited');
+  cy.get('.btn-primary').click();
+  cy.get(".table td").contains("user").should("have.text", "user edited");
+  cy.get(".alert").should("be.visible");
+  cy.get(".alert").should("contain", "User Berhasil Diupdate");
+});
+});
